dd84c13c763b0fa401284c36fac9aa09595bf75e
Messi002 <rostandmessi2@gmail.com>
2023-02-24 13:49:31 +0100

    profile_screen_controller

--- lib/Controller/profilescreen_controller.dart
+++ lib/Controller/profilescreen_controller.dart
@@ -1,14 +1,7 @@
 import 'package:get/get.dart';
 
-class ProfileController extends GetxController{
+class ProfileController extends GetxController {
+  final Rx<Map<String, dynamic>> _user = Rx<Map<String, dynamic>>({});
 
-
-
-
-
-
-
+  Map<String, dynamic> get user => _user.value;
 }
-
-
-
