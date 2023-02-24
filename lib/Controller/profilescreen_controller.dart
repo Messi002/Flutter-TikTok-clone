8f4f5a66a3d3585d2b80e271b2cbddb5af16af2c
Messi002 <rostandmessi2@gmail.com>
2023-02-24 16:08:03 +0100

    working on profile screen

--- lib/Controller/profilescreen_controller.dart
+++ lib/Controller/profilescreen_controller.dart
@@ -1,7 +1,90 @@
+import 'dart:developer';
+
+import 'package:ap2/constants.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 
 class ProfileController extends GetxController {
   final Rx<Map<String, dynamic>> _user = Rx<Map<String, dynamic>>({});
 
   Map<String, dynamic> get user => _user.value;
+
+  Rx<String> _uid = "".obs;
+
+  updateUserId(String uid) {
+    _uid.value = uid;
+  }
+
+  getUserData() async {
+    List<String> thumbnails = [];
+
+    var myVideos = await firestore
+        .collection('videos')
+        .where('uid', isEqualTo: _uid.value)
+        .get();
+
+    for (var i = 0; i < myVideos.docs.length; i++) {
+      log('${myVideos.docs[i].data()}');
+      log('${myVideos.docs[i]}');
+      log('${myVideos.docs}');
+      thumbnails.add((myVideos.docs[i].data() as dynamic)['thumbnail']);
+    }
+
+    DocumentSnapshot userDoc =
+        await firestore.collection('users').doc(_uid.value).get();
+
+    final userData = userDoc.data()! as dynamic;
+    String name = userData['name'];
+    String profilePhoto = userData['profilePhoto'];
+    int likes = 0;
+    int following = 0;
+    int followers = 0;
+    bool isFollowing = true;
+
+    for (var item in myVideos.docs) {
+      likes += (item.data()['likes'] as List).length;
+    }
+
+    var followingDoc = await firestore
+        .collection('users')
+        .doc(_uid.value)
+        .collection('following')
+        .get();
+
+    var followerDoc = await firestore
+        .collection('users')
+        .doc(_uid.value)
+        .collection('follower')
+        .get();
+
+    followers = followerDoc.docs.length;
+    following = followingDoc.docs.length;
+
+    //checking whether or not the user has followers
+    firestore
+        .collection('users')
+        .doc(_uid.value)
+        .collection('followers')
+        .doc(authController.user?.uid)
+        .get()
+        .then((value) {
+      if (value.exists) {
+        isFollowing = true;
+      } else {
+        isFollowing = false;
+      }
+    });
+
+    _user.value = {
+      'followers': followers.toString(),
+      'following': following.toString(),
+      'isFollowing': isFollowing,
+      'likes': likes.toString(),
+      'profilePhoto': profilePhoto,
+      'name': name,
+      'thumbnails': thumbnails,
+    };
+
+    update();
+  }
 }
--- lib/View/screens/profile_screen.dart
+++ lib/View/screens/profile_screen.dart
@@ -1,148 +1,162 @@
+import 'package:ap2/Controller/profilescreen_controller.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   final String uid;
-  const ProfileScreen({super.key, required this.uid});
+   ProfileScreen({super.key, required this.uid});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  final ProfileController profileController = ProfileController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black12,
-        leading: const Icon(Icons.person_add_alt_1_outlined),
-        actions: const [
-          Icon(Icons.more_horiz),
-        ],
-        title: const Text(
-          'username',
-          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
-        ),
-      ),
-      body: Column(
-        children: [
-          Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ClipOval(
-                    child: CachedNetworkImage(
-                      fit: BoxFit.cover,
-                      height: 100,
-                      width: 100,
-                      imageUrl: "",
-                      progressIndicatorBuilder:
-                          (context, url, downloadProgress) =>
-                              const CircularProgressIndicator(),
-                      // CircularProgressIndicator(value: downloadProgress.progress),
-                      errorWidget: (context, url, error) =>
-                          const Icon(Icons.error),
+    return GetBuilder<ProfileController>(
+        init: ProfileController(),
+        builder: (controller) {
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.black12,
+              leading: const Icon(Icons.person_add_alt_1_outlined),
+              actions: const [
+                Icon(Icons.more_horiz),
+              ],
+              title: const Text(
+                'username',
+                style:
+                    TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+              ),
+            ),
+            body: Column(
+              children: [
+                Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ClipOval(
+                          child: CachedNetworkImage(
+                            fit: BoxFit.cover,
+                            height: 100,
+                            width: 100,
+                            imageUrl: "",
+                            progressIndicatorBuilder:
+                                (context, url, downloadProgress) =>
+                                    const CircularProgressIndicator(),
+                            // CircularProgressIndicator(value: downloadProgress.progress),
+                            errorWidget: (context, url, error) =>
+                                const Icon(Icons.error),
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Column(
-                    children: [
-                      Text(
-                        '10',
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      Text(
-                        'Following',
-                        style: TextStyle(
-                          fontSize: 14,
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Column(
+                          children: [
+                            Text(
+                              '10',
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              'Following',
+                              style: TextStyle(
+                                fontSize: 14,
+                              ),
+                            )
+                          ],
                         ),
-                      )
-                    ],
-                  ),
-                  Container(
-                    color: Colors.black54,
-                    width: 1,
-                    height: 15,
-                    margin: const EdgeInsets.symmetric(horizontal: 15),
-                  ),
+                        Container(
+                          color: Colors.black54,
+                          width: 1,
+                          height: 15,
+                          margin: const EdgeInsets.symmetric(horizontal: 15),
+                        ),
 
-                  //2
-                  Column(
-                    children: [
-                      Text(
-                        '1',
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      Text(
-                        'Followers',
-                        style: TextStyle(
-                          fontSize: 14,
+                        //2
+                        Column(
+                          children: [
+                            Text(
+                              '1',
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              'Followers',
+                              style: TextStyle(
+                                fontSize: 14,
+                              ),
+                            )
+                          ],
                         ),
-                      )
-                    ],
-                  ),
-                  Container(
-                    color: Colors.black54,
-                    width: 1,
-                    height: 15,
-                    margin: const EdgeInsets.symmetric(horizontal: 15),
-                  ),
-                  //3
-                  Column(
-                    children: [
-                      Text(
-                        '2',
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      Text(
-                        'Likes',
-                        style: TextStyle(
-                          fontSize: 14,
+                        Container(
+                          color: Colors.black54,
+                          width: 1,
+                          height: 15,
+                          margin: const EdgeInsets.symmetric(horizontal: 15),
                         ),
-                      )
-                    ],
-                  )
-                ],
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              Container(
-                width: 140,
-                height: 47,
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black12),
+                        //3
+                        Column(
+                          children: [
+                            Text(
+                              '2',
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              'Likes',
+                              style: TextStyle(
+                                fontSize: 14,
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Container(
+                      width: 140,
+                      height: 47,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black12),
+                      ),
+                      child: Center(
+                        child: InkWell(
+                            onTap: () {},
+                            child: const Text(
+                              'Sign Out',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 15),
+                            )),
+                      ),
+                    ),
+                    //TODO: video list
+                  ],
                 ),
-                child: Center(
-                  child: InkWell(
-                      onTap: () {},
-                      child: const Text(
-                        'Sign Out',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 15),
-                      )),
-                ),
-              ),
-              //TODO: video list
-            ],
-          ),
-        ],
-      ),
-    );
+              ],
+            ),
+          );
+        });
   }
 }
